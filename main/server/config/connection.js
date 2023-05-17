48b9e9d53469c292bde4632a70107da5fde18cb3
Matthew <mduncan766@gmail.com>
2023-05-16 19:01:51 -0500

    working on exports

--- main/server/config/connection.js
+++ main/server/config/connection.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://atlas-sql-6462d9c89071120d62e9bece-wkxkx.a.query.mongodb.net/Calender?ssl=true&authSource=admin');
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
   console.log('Connected to the database!');
   // Additional code or logic to run when the connection is established
 });
+
+module.exports = db;

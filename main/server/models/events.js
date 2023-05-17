298ff48119d59c6733f081519e554daf8c2d6694
KarimB101 <karim.baptiste@yahoo.com>
2023-05-16 21:42:19 -0500

    chaneged event

--- main/server/models/events.js
+++ main/server/models/events.js
@@ -34,27 +34,3 @@ ToDoEvent
   .catch(err => handleError(err));
 
 module.exports = ToDoEvent;
-
-const mongoose = require('mongoose');
-
-const ToDoEventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  weekday: {
-    type: String,
-    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  },
-  is_completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const ToDoEvent = mongoose.model('ToDoEvent', ToDoEventSchema);
-
-module.exports = ToDoEvent;
